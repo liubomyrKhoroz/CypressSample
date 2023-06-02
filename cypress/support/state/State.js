80ca5d34137d84c63d36697e8cb505d33a58257b
liubomyrkhoroz <liubomyrkhoroz@gmail.com>
2023-06-02 16:15:00 +0300

    fix

--- cypress/support/state/State.js
+++ cypress/support/state/State.js
@@ -1,18 +1,17 @@
-import 'cypress-file-upload';
+import "cypress-file-upload";
 
 class State {
-
-	site() {
-		return cy.visit(Cypress.env('URL_MOREMD_STAGE'))//URL_AFYA_BRAIN_STAGE URL_MOREMD_STAGE
-	}
-	check() {
-		return cy.location('protocol').should("eq", 'https:')
-	}	
+  site() {
+    return cy.visit(Cypress.env("URL_MOREMD_STAGE")); //URL_AFYA_BRAIN_STAGE URL_MOREMD_STAGE
+  }
+  check() {
+    return cy.location("protocol").should("eq", "https:");
+  }
 }
-Cypress.Commands.add('forceVisit', url => {
-    cy.window().then(win => {
-        return win.open(url, '_self')
-    })
-  })
-  
+Cypress.Commands.add("forceVisit", (url) => {
+  cy.window().then((win) => {
+    return win.open(url, "_self");
+  });
+});
+
 export default State;
